3442fb05caec90422c387616197487700d63656a
LeetGameFactory <leetgamefactory@gmail.com>
2019-09-14 18:41:35 +0300

    Firebase

--- app/src/main/java/com/leetgamefactory/dilemmathegame/ui/login/LoginActivity.java
+++ app/src/main/java/com/leetgamefactory/dilemmathegame/ui/login/LoginActivity.java
@@ -4,11 +4,13 @@ import android.app.Activity;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -18,9 +20,16 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.leetgamefactory.dilemmathegame.R;
 import com.leetgamefactory.dilemmathegame.ui.login.LoginViewModel;
 import com.leetgamefactory.dilemmathegame.ui.login.LoginViewModelFactory;
+
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -31,6 +40,26 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Toast.makeText(this, "Deneme", Toast.LENGTH_SHORT).show();
+
+        FirebaseDatabase fDB = FirebaseDatabase.getInstance();
+        DatabaseReference dbRef = fDB.getReference();
+
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                    String value = child.getValue(String.class);
+                    Log.d("AA", "Value is: " + value);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
         loginViewModel = ViewModelProviders.of(this, new LoginViewModelFactory())
                 .get(LoginViewModel.class);
 
@@ -38,7 +67,6 @@ public class LoginActivity extends AppCompatActivity {
         final EditText passwordEditText = findViewById(R.id.password);
         final Button loginButton = findViewById(R.id.login);
         final ProgressBar loadingProgressBar = findViewById(R.id.loading);
-
 
 
         loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
